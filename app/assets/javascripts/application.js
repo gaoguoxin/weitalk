4fe512f8cadae34b65c9d82fd9aef5428f9c7efd
Davis Gao <naitnix@126.com>
2015-06-12 12:51:58 +0800

    change style

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,7 @@
 	  			var w = $(this).width() ;
 	  			$(this).css("height",w);
 	  		});
+			var timer;
 			$("#player1")[0].ifplay=0;
 			$("#player2")[0].ifplay=0;
 			$("#player3")[0].ifplay=0;
@@ -31,115 +32,78 @@
 			$("#player10")[0].ifplay=0;
 			$("#player11")[0].ifplay=0;
 			$("#player12")[0].ifplay=0;
-	  		$("#audio1").click(function(event) {
-	  			/* Act on the event */
-	  			if ($("#player1")[0].ifplay==0){
-	  				$("#player1")[0].play();
-	  				$("#player1")[0].ifplay=1;
-	  			}else{
-	  				$("#player1")[0].pause();
-	  				$("#player1")[0].ifplay=0;
+			
+			function init(){
+				$("#audio1").attr("src","/assets/talk4.png")
+				$("#audio2").attr("src","/assets/talk4.png")
+				$("#audio3").attr("src","/assets/talk1.png")
+				$("#audio5").attr("src","/assets/talk4.png")
+				$("#audio6").attr("src","/assets/talk4.png")
+				$("#audio7").attr("src","/assets/talk1.png")
+				$("#audio8").attr("src","/assets/talk1.png")
+				$("#audio9").attr("src","/assets/talk1.png")
+				$("#audio10").attr("src","/assets/talk1.png")
+				$("#audio11").attr("src","/assets/talk4.png")
+				$("#audio12").attr("src","/assets/talk4.png")
+			}
+			function talking(s){
+				if ($(s).attr("src")=="/assets/talk1.png")
+				{
+					$(s).attr("src","/assets/talk2.png");
+				}else if ($(s).attr("src")=="/assets/talk2.png")
+				{
+					$(s).attr("src","/assets/talk3.png");
+				}else if ($(s).attr("src")=="/assets/talk3.png")
+				{
+					$(s).attr("src","/assets/talk1.png");
+				}	
+				if ($(s).attr("src")=="/assets/talk4.png")
+				{
+					$(s).attr("src","/assets/talk5.png");
+				}else if ($(s).attr("src")=="/assets/talk5.png")
+				{
+					$(s).attr("src","/assets/talk6.png");
+				}else if ($(s).attr("src")=="/assets/talk6.png")
+				{
+					$(s).attr("src","/assets/talk4.png");
+				}
+			}
+			function allstop(){
+				$("#player1")[0].pause();$("#player1")[0].ifplay=0;
+				$("#player2")[0].pause();$("#player2")[0].ifplay=0;
+				$("#player3")[0].pause();$("#player3")[0].ifplay=0;
+				$("#player5")[0].pause();$("#player5")[0].ifplay=0;
+				$("#player6")[0].pause();$("#player6")[0].ifplay=0;
+				$("#player7")[0].pause();$("#player7")[0].ifplay=0;
+				$("#player8")[0].pause();$("#player8")[0].ifplay=0;
+				$("#player9")[0].pause();$("#player9")[0].ifplay=0;
+				$("#player10")[0].pause();$("#player10")[0].ifplay=0;
+				$("#player11")[0].pause();$("#player11")[0].ifplay=0;
+				$("#player12")[0].pause();$("#player12")[0].ifplay=0;
+			}
+
+			function doclick(num){
+				var s="#audio"+num;
+				var ss="#player"+num;
+				$(s).click(function(event) {
+		  			if ($(ss)[0].ifplay==0){
+		  				allstop();
+						clearInterval(timer);init();
+						timer = setInterval(function(){talking(s)},300);
+		  				$(ss)[0].play();
+		  				$(ss)[0].ifplay=1;
+		  			}else{
+		  				allstop();
+						clearInterval(timer);
+						init();
+		  			}
+		  		});
+			}
+	  		for(var i=1;i<13;i++)
+	  		{
+	  			if (i != 4){
+	  				doclick(i);
 	  			}
-	  		});
-	  		$("#audio2").click(function(event) {
-	  			/* Act on the event */
-	  			if ($("#player2")[0].ifplay==0){
-	  				$("#player2")[0].play();
-	  				$("#player2")[0].ifplay=1;
-	  			}else{
-	  				$("#player2")[0].pause();
-	  				$("#player2")[0].ifplay=0;
-	  			}
-	  		});
-	  		$("#audio3").click(function(event) {
-	  			/* Act on the event */
-	  			if ($("#player3")[0].ifplay==0){
-	  				$("#player3")[0].play();
-	  				$("#player3")[0].ifplay=1;
-	  			}else{
-	  				$("#player3")[0].pause();
-	  				$("#player3")[0].ifplay=0;
-	  			}
-	  		});
-	  		$("#audio5").click(function(event) {
-	  			/* Act on the event */
-	  			if ($("#player5")[0].ifplay==0){
-	  				$("#player5")[0].play();
-	  				$("#player5")[0].ifplay=1;
-	  			}else{
-	  				$("#player5")[0].pause();
-	  				$("#player5")[0].ifplay=0;
-	  			}
-	  		});
-	  		$("#audio6").click(function(event) {
-	  			/* Act on the event */
-	  			if ($("#player6")[0].ifplay==0){
-	  				$("#player6")[0].play();
-	  				$("#player6")[0].ifplay=1;
-	  			}else{
-	  				$("#player6")[0].pause();
-	  				$("#player6")[0].ifplay=0;
-	  			}
-	  		});
-	  		$("#audio7").click(function(event) {
-	  			/* Act on the event */
-	  			if ($("#player7")[0].ifplay==0){
-	  				$("#player7")[0].play();
-	  				$("#player7")[0].ifplay=1;
-	  			}else{
-	  				$("#player7")[0].pause();
-	  				$("#player7")[0].ifplay=0;
-	  			}
-	  		});
-	  		$("#audio8").click(function(event) {
-	  			/* Act on the event */
-	  			if ($("#player8")[0].ifplay==0){
-	  				$("#player8")[0].play();
-	  				$("#player8")[0].ifplay=1;
-	  			}else{
-	  				$("#player8")[0].pause();
-	  				$("#player8")[0].ifplay=0;
-	  			}
-	  		});
-	  		$("#audio9").click(function(event) {
-	  			/* Act on the event */
-	  			if ($("#player9")[0].ifplay==0){
-	  				$("#player9")[0].play();
-	  				$("#player9")[0].ifplay=1;
-	  			}else{
-	  				$("#player9")[0].pause();
-	  				$("#player9")[0].ifplay=0;
-	  			}
-	  		});
-	  		$("#audio10").click(function(event) {
-	  			/* Act on the event */
-	  			if ($("#player10")[0].ifplay==0){
-	  				$("#player10")[0].play();
-	  				$("#player10")[0].ifplay=1;
-	  			}else{
-	  				$("#player10")[0].pause();
-	  				$("#player10")[0].ifplay=0;
-	  			}
-	  		});
-	  		$("#audio11").click(function(event) {
-	  			/* Act on the event */
-	  			if ($("#player11")[0].ifplay==0){
-	  				$("#player11")[0].play();
-	  				$("#player11")[0].ifplay=1;
-	  			}else{
-	  				$("#player11")[0].pause();
-	  				$("#player11")[0].ifplay=0;
-	  			}
-	  		});
-	  		$("#audio12").click(function(event) {
-	  			/* Act on the event */
-	  			if ($("#player12")[0].ifplay==0){
-	  				$("#player12")[0].play();
-	  				$("#player12")[0].ifplay=1;
-	  			}else{
-	  				$("#player12")[0].pause();
-	  				$("#player12")[0].ifplay=0;
-	  			}
-	  		});
+	  		}
 
 		});
